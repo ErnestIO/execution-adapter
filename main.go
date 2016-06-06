6f9b105ef0cc587392f6151232ce4efb288fe015
Adria Cidre <adria.cidre@gmail.com>
2016-06-06 11:17:55 +0200

    Initial comit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"runtime"
+	"time"
+
+	"github.com/nats-io/nats"
+	o "github.com/r3labs/otomo"
+)
+
+var nc *nats.Conn
+var natsErr error
+
+func getConnectorTypes(ctype string) []string {
+	var connectors map[string][]string
+
+	resp, err := nc.Request("config.get.connectors", nil, time.Second)
+	if err != nil {
+		log.Println("could not get config for connectors")
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(resp.Data, &connectors)
+	if err != nil {
+		log.Println("could not read config response")
+		log.Fatal(err)
+	}
+
+	if connectors[ctype] == nil {
+		log.Fatal("connector type not found")
+	}
+
+	return connectors[ctype]
+}
+
+func main() {
+	natsURI := os.Getenv("NATS_URI")
+	if natsURI == "" {
+		natsURI = nats.DefaultURL
+	}
+
+	nc, natsErr = nats.Connect(natsURI)
+	if natsErr != nil {
+		log.Fatal(natsErr)
+	}
+
+	c := o.Config{
+		Client:     nc,
+		ValidTypes: getConnectorTypes("executions"),
+	}
+
+	log.Println("Set up executions")
+	o.StandardSubscription(&c, "execution.create", "execution_type")
+
+	runtime.Goexit()
+}
